6df7e6b2c34f2444a4281954fd770b96f06c216a
nadermay <maynader08@gmail.com>
2024-11-27 16:30:38 +0100

    done adding http

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 import express from "express";
 import {PORT} from "./config.js"
+
 const app = express();
+
+app.get('/',(request,response)=>{
+    console.log(request)
+    return response.status(234).send('welcome to books store')
+    ;
+});
+
 app.listen(PORT,()=>{
     console.log(`App is listening to psot : ${PORT}`);
 })
