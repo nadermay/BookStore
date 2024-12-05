78d7948534be437720a44851ebd0fac6e7046d74
nadermay <maynader08@gmail.com>
2024-12-05 15:24:03 +0100

    just add the delet update and add book function and route

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express, { request, response } from "express";
+import express, { json, request, response } from "express";
 import {PORT,mongoDBREL} from "./config.js";
 import mongoose from 'mongoose';
 import { Book} from './models/bookModel.js';
@@ -49,6 +49,78 @@ app.post('/books', async (request, response) => {
     }
   });
   
+
+// route for get all the books 
+app.get('/books',async(request,response)=>{
+  try{
+    const books = await Book.find({});
+    return response.status(200).json({
+      count : books.length,
+      data : books
+    });
+  }catch(error){
+    console.log(error.message);
+    response.status(500).send({message:error.message})
+  }
+})
+// route for get all the books  by id 
+app.get('/books/:id',async(request,response)=>{
+  try{
+
+    const{id}=request.params;
+
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+  }catch(error){
+    console.log(error.message);
+    response.status(500).send({message:error.message})
+  }
+})
+
+//update book route
+app.put('/books/:id',async(request,response)=>{
+  try{
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ){
+      return response.status(400).send({
+        message:'send all required fields : title,author; publishYear'
+      })
+    }
+    const {id}= request.params;
+    const result = await Book.findByIdAndUpdate(id,request.body);
+if(!result){
+  return response.status(404).json({message:'book not found'});
+}
+return response.status(200).send({message:'Book updated successfully'})
+  }catch(error) {
+    console.log(error.message);
+    response.status(500).send({message:error.message});
+  }
+})
+
+// delete book route
+app.delete('/books/:id',async(request,response)=>{
+  try{
+    const{id}=request.params;
+    const result = await Book.findByIdAndDelete(id);
+
+      if(!result){
+        return response.status(404).json({message:'book not found'})
+      }
+
+      return response.status(200).send({message:'book deleted successfully'})
+
+  }catch(error){
+console.log(error.message);
+response.status(500).send({message:error.message})
+  }
+})
+
+
+
 mongoose
 .connect(mongoDBREL)
 .then(()=>{
